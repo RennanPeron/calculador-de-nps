caf103d00e2e83830efe1d79de9de7f744dad51a
RennanPeron <rennanpdc@gmail.com>
2021-02-23 17:36:07 -0300

    database, controllers and models

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database/index'
+import {router} from './routes'
 
 const app = express()
 
-app.get("/", (req, res) => {
-    return res.json({message: "Ola Mundo - NLW04"})
-})
-
-app.post('/', (req, res) => {
-    return res.json({message: "Os dados foram salvos."})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3000, () => {
     console.log("Server is Running!")
