ecdad3591d73e1aa6323da2a35ac7f9764968aab
RennanPeron <rennanpdc@gmail.com>
2021-02-24 16:12:53 -0300

    survey added

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import {Router} from 'express'
 import { UserController } from './controllers/UserController'
+import { SurveysController } from './controllers/SurveyController'
 
 const router = Router()
 
 const userController = new UserController()
+const surveyController = new SurveysController()
 
 router.post("/users", userController.create)
+router.get("/surveys", surveyController.show)
+router.post("/surveys", surveyController.create)
+
 
 export {router}
